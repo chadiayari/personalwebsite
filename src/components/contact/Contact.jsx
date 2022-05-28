5dd2f0d15019de3f6d66fdd37273fedb7a278582
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2022-05-28 19:24:22 +0530

    edited title tag index

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,92 +1,21 @@
 import React from "react";
-import axios from "axios";
+import { useForm } from "react-hook-form";
 
-function ContactForm() {
-  // isSubmitting = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    axios({
-      method: "POST",
-      url: "https://www.chadiayari.com/send",
-      data: {
-        name: name,
-        email: email,
-        messsage: message,
-      },
-    }).then((response) => {
-      if (response.data.msg === "success") {
-        alert("Message Sent, I will get back to you shortly!");
-        resetForm();
-      } else if (response.data.msg === "fail") {
-        alert("Message failed to send, please try again.");
-      }
-    });
-  };
-
-  const resetForm = () => {
-    document.getElementById("contact-form").reset();
-  };
-
+const Contact = () => {
   return (
     <>
-      <form id="contact-form" onSubmit={handleSubmit} method="POST">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="form-group">
-              <input
-                type="text"
-                id="name"
-                className="form-control"
-                placeholder="Full name"
-              />
-              <label className="form-label">Name</label>
-            </div>
-          </div>
-          {/* End .col-6 */}
-
-          <div className="col-md-6">
-            <div className="form-group">
-              <input
-                type="email"
-                id="email"
-                className="form-control"
-                placeholder="Email address"
-              />
-              <label className="form-label">Email</label>
-            </div>
-          </div>
-          {/* End .col-6 */}
-
-          <div className="col-12">
-            <div className="form-group">
-              <textarea
-                rows="4"
-                className="form-control"
-                placeholder="Type comment"
-                id="message"
-              ></textarea>
-              <label className="form-label">Comment</label>
-            </div>
-          </div>
-          {/* End .col-12 */}
-
-          <div className="col-12">
-            <div className="btn-bar">
-              <button type="submit" className="px-btn px-btn-theme">
-                Send your message
-              </button>
-            </div>
-          </div>
-
-          {/* End .col-12 */}
+      <div className="col-12">
+        <div className="btn-bar mt-4">
+          <a
+            className="px-btn px-btn-theme btn-md"
+            href="mailto:info@chadiayari.com"
+          >
+            Send me a message
+          </a>
         </div>
-      </form>
+      </div>
     </>
   );
-}
+};
 
-export default ContactForm;
+export default Contact;
